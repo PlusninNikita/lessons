2d3a3e81b154d6812e66fb1765e60cd3ad2d12f8
NikitaPlusnin <plusnin@mail.com>
2023-09-12 20:56:22 +0500

    work

---
+++ task_34/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int CountEvenNumbersArray(int[] arr, bool even)
+{
+    int countEven = 0;
+    int countOdd = 0;
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 1)
+        {
+            countEven++;
+        }
+        else
+        {
+            countOdd++;
+        }        
+    }
+    return result = even ? countEven : countOdd;
+    // someValue = condition ? newValue : someOtherValue;
+}
+
+int[] array = CreateArrayRndInt(5, 100, 999);
+PrintArray(array);
+Console.WriteLine();
+int countEvenNumbers = CountEvenNumbersArray(array, true);
+Console.WriteLine($"Количество нечётных чисел в массиве: {countEvenNumbers}");
---
+++ task_38/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.NextDouble() * (max - min) + min;
+    }
+    return arr;
+}
+
+void PrintArrayDouble(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]:F1}, ");
+        else Console.Write($"{arr[i]:F1}");
+    }
+    Console.Write("]");
+}
+
+double DifferenceMaxMin(double[] arr)
+{
+    double maxNumber = arr[0];
+    double minNumber = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > maxNumber)
+        {
+            maxNumber = arr[i];
+        }
+        if (arr[i] < minNumber)
+        {
+            minNumber = arr[i];
+        }
+    }
+    Console.WriteLine($"Минимальное значение {minNumber:F1}");
+    Console.WriteLine($"Максимальное значение {maxNumber:F1}");
+    return maxNumber - minNumber;
+}
+
+void StartDifferenceMaxMin()
+{   
+    Console.Clear();
+    Console.WriteLine("Введите три параметра генерации массива");
+    Console.Write("1. Введите размер массива: ");
+    int sizeArray = Convert.ToInt32(Console.ReadLine());
+    Console.Write("2. Минимальное значение : ");
+    int minNum = Convert.ToInt32(Console.ReadLine());
+    Console.Write("3. Максимальное значение: ");
+    int maxNum = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("------------------------");
+    Console.WriteLine($"Параметры генерации массива: размер {sizeArray}, диапазон значений {minNum}-{maxNum}");
+    Console.WriteLine("------------------------");
+
+    double[] array = CreateArrayRndDouble(sizeArray, minNum, maxNum);
+    PrintArrayDouble(array);
+    Console.WriteLine();    
+    Console.WriteLine();
+    double difference = (DifferenceMaxMin(array));
+    Console.WriteLine($"Разница между максимальным и минимальным элементами массива {difference:F1}");
+}
+
+StartDifferenceMaxMin();
